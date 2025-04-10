56999b2104a3e828c26be27a54b5b6f4622b39b1
Sharathguru <vsarath200219@gmail.com>
2025-04-10 20:52:23 +0530

    Updated content

--- Day7 with ejs_data and urlencoded/.history/DB/task.DB_20250410170005.js
+++ Day7 with ejs_data and urlencoded/.history/DB/task.DB_20250410170005.js
@@ -4,7 +4,8 @@ import { connect } from "mongoose";
 async function dB() {
     try {
         await connect("mongodb+srv://skv200219:qJf4LZXoFui7WtdR@cluster0.owgywmv.mongodb.net/toduDB1?retryWrites=true&w=majority&appName=Cluster0", {
-         
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
         console.log("DB Connected Successfully!");
     } catch (error) {
